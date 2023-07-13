a65bf99f9207da9e48e5fc9c7989d3f08cba985b
Ajinkyab2001 <ajinkyab2001@gmail.com>
2023-07-13 14:19:31 +0530

    Conditional Rendering

--- src/components/Expenses/expenses.js
+++ src/components/Expenses/expenses.js
@@ -15,14 +15,50 @@ const Expenses = (props) => {
       return expense.date.getFullYear().toString() === filteredYear;
     });
 
+   let expensesContent = <p className='noExpense'>No expense added</p>
+   let singleExpense = <p className='noExpense'>Only single Expense here. Please add more..</p>
+   if(filteredExpenses.length  === 1){
+    expensesContent = filteredExpenses.map((e,i)=>{
+      return <ExpenseItem key={e.id} id={e.id} title={e.title} amount={e.amount} date={e.date} location={e.location} />
+    })
+   
+    expensesContent = [...expensesContent, singleExpense]
+
+   }else if(filteredExpenses.length  > 0){
+    expensesContent = filteredExpenses.map((e,i)=>{
+      return <ExpenseItem key={e.id} id={e.id} title={e.title} amount={e.amount} date={e.date} location={e.location} />
+    })
+   }
+
+
   return (
     <div className='expenses'>
     <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-         {
+    {expensesContent}
+
+    {/* another way of checking if the list is empty or not */}
+    {/* {filteredExpenses.length === 0 && <p>no expense added</p>}
+    {filteredExpenses.length > 0 &&  filteredExpenses.map((e,i)=>{
+        return <ExpenseItem key={e.id} id={e.id} title={e.title} amount={e.amount} date={e.date} location={e.location} />
+      })} */}
+
+
+
+    {/* using ternary operator to check if the list is empty */}
+    {/* {
+      filteredExpenses.length ===0 ? <p>No expense added</p> :  filteredExpenses.map((e,i)=>{
+        return <ExpenseItem key={e.id} id={e.id} title={e.title} amount={e.amount} date={e.date} location={e.location} />
+      })
+    } */}
+
+
+
+    {/* simple filter func */}
+         {/* {
       filteredExpenses.map((e,i)=>{
         return <ExpenseItem key={e.id} id={e.id} title={e.title} amount={e.amount} date={e.date} location={e.location} />
       })
-    }
+    } */}
     </div>
   )
 }
